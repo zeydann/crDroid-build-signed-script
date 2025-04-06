7985f14148adbd3d62f349549a45183db102b866
Zeyy <zaidanagusta319@gmail.com>
2025-04-06 16:21:08 +0700

    update script

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -37,10 +37,10 @@ done
 
 
 ## Create vendor for keys
-mkdir -p vendor/lineage-priv
-mv ~/.android-certs vendor/lineage-priv/keys
-echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
-cat <<EOF > vendor/lineage-priv/keys/BUILD.bazel
+mkdir -p vendor/aosp-priv
+mv ~/.android-certs vendor/aosp-priv/keys
+echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aosp-priv/keys/releasekey" > vendor/aosp-priv/keys/keys.mk
+cat <<EOF > vendor/aosp-priv/keys/BUILD.bazel
 filegroup(
     name = "android_certificate_directory",
     srcs = glob([
@@ -51,6 +51,6 @@ filegroup(
 )
 EOF
 
-echo "Done! Now build as usual. If builds aren't being signed, add '-include vendor/lineage-priv/keys/keys.mk' to your device mk file"
-echo "Make copies of your vendor/lineage-priv folder as it contains your keys!"
+echo "Done! Now build as usual. If builds aren't being signed, add '-include vendor/aosp-priv/keys/keys.mk' to your device mk file"
+echo "Make copies of your vendor/aosp-priv folder as it contains your keys!"
 sleep 3
